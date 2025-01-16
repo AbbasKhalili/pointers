480a26da589dd98fafdfbee23abd8ca26f2a9311
Abbas Khalili <khalilikhoo2@hotmail.com>
2025-01-16 20:52:42 +0330

    pointer type

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+type Car struct {
+	brandId uint16
+	modelId uint32
+	trimId  *uint32
+}
+
+func main() {
+	var age uint8 = 40
+	var agePtr *uint8 = &age
+
+	fmt.Println("Age is : ", age)
+	fmt.Println("Age ptr is : ", agePtr)
+
+	PrintAge(&age)
+
+	fmt.Println("Age is : ", age)
+	fmt.Println("Age ptr is : ", agePtr)
+}
+
+func PrintAge(age *uint8) {
+	fmt.Println("Age is : ", *age)
+	fmt.Println("Age address is : ", age)
+	fmt.Println("Age ptr is : ", &age)
+
+	*age = 45
+
+	fmt.Println("Age is : ", *age)
+	fmt.Println("Age address is : ", age)
+	fmt.Println("Age ptr is : ", &age)
 }
